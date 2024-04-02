1d989e78aeb116aab47f9d24923d6c116dead22e
JoannaPokora <pokorajo@gmail.com>
2024-04-02 11:42:00 +0200

    moved get_LOD_to_remove to raw_data_funs

--- R/raw_data_funs.R
+++ R/raw_data_funs.R
@@ -70,22 +70,22 @@ get_info <- function(dat){
   
 }
 
-#' Print Biocrates data
-#' 
-#' @param dat a \code{\link{raw_data}} object. Output of [read_data()] function.
-#' 
-#' @seealso [read_data()]
-#' 
-#' @examples
-#' path <- get_example_data("small_biocrates_example.xls")
-#' dat <- read_data(path)
-#' print(dat)
-#' 
-#' dat <- add_group(dat, "group")
-#' print(dat)
-#' 
-#' @export
-#' 
+# Print Biocrates data
+# 
+# @param dat a \code{\link{raw_data}} object. Output of [read_data()] function.
+# 
+# @seealso [read_data()]
+# 
+# @examples
+# path <- get_example_data("small_biocrates_example.xls")
+# dat <- read_data(path)
+# print(dat)
+# 
+# dat <- add_group(dat, "group")
+# print(dat)
+# 
+# @export
+# 
 # print.raw_data <- function(dat){
 #   
 #   if(any(class(dat) != c("raw_data", "data.frame")))
@@ -119,6 +119,32 @@ get_info <- function(dat){
 #              "\nShowing ", ratio_rows, " out of ", nrow(attr(dat, "NA_info")$NA_ratios), " rows"))
 #   
 # }
+
+
+#' Get metabolites to remove
+#'
+#' @description Returns metabolite names having more NA values in each group than the given treshold.
+#' 
+#' @param NA_info Attribute of the raw_data object.
+#' @param treshold Percentage value.
+#' 
+#' @examples
+#' path <- get_example_data("small_biocrates_example.xls")
+#' dat <- read_data(path)
+#' get_LOD_to_remove(attr(dat, "NA_info"), 0.1)
+#' 
+#' @export
+#'
+
+get_LOD_to_remove <- function(NA_info, treshold){
+  
+  NA_info$NA_ratios %>%
+    group_by(metabolite) %>% 
+    filter(all(NA_frac > treshold)) %>%
+    distinct(metabolite) %>% 
+    pull(metabolite)
+  
+}
 
 
 #' Adding metabolites to the attribute removed
