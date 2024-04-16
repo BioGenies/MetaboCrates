b4f71161deabbc034d5b6fb3be3676a2ae145a58
Filip Gaj <f.gaj16@gmail.com>
2024-04-16 12:11:36 +0200

    tests for remove_metabolites and unremove_metabolites functions

---
+++ tests/testthat/test-raw_darta_funs.R
@@ -0,0 +1,24 @@
+library(testthat)
+
+path <- get_example_data("small_biocrates_example.xls")
+test_dat <- read_data(path)
+
+test_that("remove_metabolites dodaje metabolity do atrybutu removed", {
+  metabolites_to_remove <- c("C0", "C1", "C2")
+  type <- "LOD"
+  
+  modified_dat <- remove_metabolites(test_dat, metabolites_to_remove, type)
+  
+  expect_equal(attr(modified_dat, "removed")[[type]], metabolites_to_remove)
+})
+
+
+test_that("unremove_metabolites usuwa metabolity z atrybutu removed", {
+  metabolites_to_remove <- c("C0", "C1", "C2")
+  type <- "LOD"
+  test_dat <- remove_metabolites(test_dat, metabolites_to_remove, type)
+  
+  modified_dat <- unremove_metabolites(test_dat, type)
+  
+  expect_equal(attr(modified_dat, "removed")[[type]], NULL)
+})
