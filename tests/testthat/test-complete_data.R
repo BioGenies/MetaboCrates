bf3fa4fa1fa3c87815278b7aa585a019c2b00dd3
KrystynaGrzesiak <krygrz11@gmail.com>
2024-04-26 17:36:22 +0200

    adds tests

---
+++ tests/testthat/test-complete_data.R
@@ -0,0 +1,66 @@
+library(testthat)
+
+
+path <- get_example_data("small_biocrates_example.xls")
+dat <- read_data(path)
+
+dat_completed <- complete_data(dat, LOD_method = "limit", LLOQ_method = "limit", 
+                               ULOQ_method = "limit")
+
+completed <- attributes(dat_completed)[["completed"]]
+
+
+test_that("Attribute completed is added to the data", {
+  expect_true(all(dim(completed) == c(41, 27)))
+  expect_s3_class(completed, c("raw_data", "data.frame"))
+})
+
+test_that("All the values are completed", {
+  expect_false(any(completed == "< LOD", na.rm = TRUE))
+  expect_false(any(completed == "< LLOQ", na.rm = TRUE))
+  expect_false(any(completed == "> ULOQ", na.rm = TRUE))
+})
+
+test_that("Proper LOD values were imputed", {
+  expect_equal(completed[2, 21], 0.17)
+  expect_equal(completed[3, 21], 0.17)
+  expect_equal(completed[2, 22], 0.019)
+  expect_equal(completed[1, 23], NA_real_)
+})
+
+test_that("Proper LLOQ values were imputed", {
+  expect_equal(completed[19, 22], 0.4)
+  expect_equal(completed[19, 24], 0.1)
+})
+
+test_that("Proper ULOQ values were imputed", {
+  expect_equal(completed[28, 19], 100)
+  expect_equal(completed[39, 20], 10
+  )
+})
+
+
+dat_completed <- complete_data(dat, LOD_method = "random")
+
+test_that("Imputation of only LODs", {
+  expect_equal(sum(dat_completed == "< LLOQ", na.rm = TRUE), 6)
+  expect_equal(sum(dat_completed == "> ULOQ", na.rm = TRUE), 11)
+})
+
+dat_completed <- complete_data(dat, LLOQ_method = "limit")
+
+test_that("Imputation of only LODs", {
+  expect_equal(sum(dat_completed == "< LOD", na.rm = TRUE), 132)
+  expect_equal(sum(dat_completed == "> ULOQ", na.rm = TRUE), 11)
+})
+
+dat_completed <- complete_data(dat, ULOQ_method = "limit")
+
+test_that("Imputation of only LODs", {
+  expect_equal(sum(dat_completed == "< LOD", na.rm = TRUE), 132)
+  expect_equal(sum(dat_completed == "> LLOQ", na.rm = TRUE), 6)
+})
+
+
+
+
