7123191c1f670cd0c9cc0e2b1086d9f942d81912
KrystynaGrzesiak <krygrz11@gmail.com>
2024-05-21 17:38:43 +0200

    first draft

--- inst/metabocrates/app.R
+++ inst/metabocrates/app.R
@@ -1,11 +1,42 @@
 library(MetaboCrates)
 
 library(shiny)
+library(shinythemes)
+library(shinyWidgets)
+library(shinycssloaders)
+library(DT)
+library(shinyhelper)
 
 
-ui <- fluidPage(
+ui <- navbarPage(
+  theme = shinytheme("slate"),
+  title = "metabocrates",
+  tabPanel("About",
+           column(1, HTML("<img src='logo.jpg' height='120px'>")),
+           column(9,
+                  h1("Welcome! This is metabocrates app!"),
+                  h3("You can do here some cool stuff with your biocrates data"),
+                  h3("Click `start` button and start your analysis!")),
+           column(2, 
+                  br(),
+                  actionBttn(
+                    inputId = "Id105",
+                    label = "START",
+                    style = "pill", 
+                    color = "success",
+                    icon = icon("arrow-right"),
+                    size = "lg"
+                  ))
+  ),
+  tabPanel("Uploading data"),
+  tabPanel("Missing values analysis"),
+  tabPanel("Quality control"),
+  tabPanel("Summary"),
+  tabPanel("Download")
+  
   
 )
+
 
 server <- function(input, output, session) {
   
