232cba175a8075c15c3e8a641cc84bf23795a50f
KrystynaGrzesiak <krygrz11@gmail.com>
2024-05-22 13:51:08 +0200

    app update

---
+++ inst/drafts.R
@@ -0,0 +1,5 @@
+
+path <- get_example_data("small_biocrates_example.xls")
+read_data(path)
+
+
---
+++ inst/metabocrates/app_supplementary/custom_dt.R
@@ -0,0 +1,15 @@
+
+custom_datatable <- function(dat,
+                             paging = TRUE,
+                             scrollY = 380,
+                             selection = list(selectable = FALSE)) {
+  DT::datatable(dat,
+                editable = FALSE,
+                selection = selection,
+                options = list(paging = paging,
+                               scrollX = TRUE,
+                               scrollY = scrollY,
+                               pageLength = 15,
+                               searching = FALSE),
+                class = "display nowrap",)
+}
