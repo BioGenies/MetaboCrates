a75ee006c4959754421840c518824f8ffcfb4be2
JoannaPokora <pokorajo@gmail.com>
2024-09-04 14:41:56 +0200

    added module for table with button

---
+++ inst/metabocrates/app_supplementary/table_with_button_module.R
@@ -0,0 +1,35 @@
+table_with_button_UI <- function(id){
+  ns <- NS(id)
+  
+  fluidRow(
+    column(11,
+           helper(DTOutput(ns("table")),
+                  type = "markdown",
+                  content = id)
+           ),
+    column(1,
+           downloadBttn(
+             ns("download_button"),
+             style = "jelly",
+             label = NULL,
+             color = "primary",
+             icon = icon("download")
+           )
+           )
+    )
+}
+
+table_with_button_SERVER <- function(id, dat_reactive){
+  moduleServer(id, function(input, output, session){
+    output[["table"]] <- renderDT(dat_reactive())
+    
+    output[["download_button"]] <- downloadHandler(
+      filename = function(){
+        paste0(switch(id,
+                      NA_counts = "NA_counts"), ".csv")
+      },
+      content =
+        function(file) write.csv(dat_reactive(), file, row.names = FALSE)
+    )
+  })
+}
