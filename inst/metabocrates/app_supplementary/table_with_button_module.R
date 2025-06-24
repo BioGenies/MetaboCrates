3614a08b8fd2dc77037ea4dbc3c13812287153dd
JoannaPokora <pokorajo@gmail.com>
2025-06-24 14:42:51 +0200

    cleaned table module

--- inst/metabocrates/app_supplementary/table_with_button_module.R
+++ inst/metabocrates/app_supplementary/table_with_button_module.R
@@ -14,14 +14,5 @@ table_with_button_SERVER <- function(id, dat_reactive){
     }
     
     output[["table"]] <- renderDT(dat_reactive())
-    
-    output[["download_button"]] <- downloadHandler(
-      filename = function(){
-        paste0(switch(id,
-                      NA_counts = "NA_counts"), ".csv")
-      },
-      content =
-        function(file) write.csv(dat_reactive(), file, row.names = FALSE)
-    )
   })
 }
