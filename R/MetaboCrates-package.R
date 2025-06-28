cd830932e15fd3c62f7209395ccc7fc39ee4c417
JoannaPokora <pokorajo@gmail.com>
2025-06-28 12:23:29 +0200

    added global variables

--- R/MetaboCrates-package.R
+++ R/MetaboCrates-package.R
@@ -9,12 +9,13 @@ NULL
 
 if(getRversion() >= "2.15.1")  
   utils::globalVariables(
-    c("after", "before", "col_type", "Component", "compound", "Count", 
-                           "Cumulative_Variance", "CV", "density", "group_name", "group_tmp", 
-                           "grouping_column", "linetype", "max_density", "measurement time", 
-                           "metabolite", "Metabolite", "model", "NA_frac", "observed", "Observed", 
-                           "PC1", "PC2", "plate bar code", "probs", "sample", "Sample", 
-                           "sample identification", "sample type", "thresh_est", "tmp_id", 
-                           "tooltip", "tooltip", "total_NA_frac", "type", "Type", "value", 
-                           "Value", "Var1", "Var2", "Variable", "Variance_Explained", "xintercept")
+    c("after", "before", "col_type", "Component", "compound", "Count",
+      "Cumulative_Variance", "CV", "density", "group_name", "group_tmp", 
+      "grouping_column", "linetype", "max_density", "measurement time", 
+      "metabolite", "Metabolite", "model", "NA_frac", "observed", "Observed", 
+      "PC1", "PC2", "plate bar code", "probs", "sample", "Sample", 
+      "sample identification", "sample type", "thresh_est", "tmp_id", 
+      "tooltip", "tooltip", "total_NA_frac", "type", "Type", "value", 
+      "Value", "Var1", "Var2", "Variable", "Variance_Explained", "xintercept",
+      "max_cor", "color", "label")
     )
